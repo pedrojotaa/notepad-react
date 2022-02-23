ee919136671eb95210013e64679113ca10fdaf3d
Joao Pedro <eng.joao.pedro@outlook.com>
2022-02-23 12:00:31 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+
+function App() { 
+  return (
+    <section>
+      <FormularioCadastro/>
+      <ListaDeNotas/>
+    </section>
+    
+  );
+}
+
+export default App;
