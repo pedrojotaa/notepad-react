8bdd4e4eebc7248727df193e57df07bd7442094c
Joao Pedro <eng.joao.pedro@outlook.com>
2022-03-02 12:46:15 -0300

    deleting a card

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -9,7 +9,12 @@ class ListaDeNotas extends Component {
                 {this.props.notas.map((nota, index) => {
                     return(
                         <li key={index} className="lista-notas_item">
-                            <CardNota titulo = {nota.titulo} texto = {nota.texto}/>
+                            <CardNota
+                                indice = {index}
+                                apagarNota = {this.props.apagarNota} 
+                                titulo = {nota.titulo} 
+                                texto = {nota.texto}
+                            />
                         </li>
                     )
                 })}
